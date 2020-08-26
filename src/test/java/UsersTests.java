0ac2a85edc6f67c27161035734a4b7573be60eec
nguyenvictor10001@gmail.com <nguyenvictor10001@gmail.com>
2020-08-26 19:54:08 -0400

    testing

--- src/test/java/UsersTests.java
+++ src/test/java/UsersTests.java
@@ -6,17 +6,17 @@ import java.net.URISyntaxException;
 
 public class UsersTests {
 
-//    UsersController uc = new UsersController();
-//
-//    @Test
-//    public void checkUserAdded() throws URISyntaxException {
-//        Users u = new Users();
-//        Assert.assertFalse(uc.addUser(u));
-//        u.setEmail("testtwo@host.com");
-//        u.setPassword("password");
-//        Assert.assertFalse(uc.addUser(u));
-//    }
-//
-//
+    UsersController uc = new UsersController();
+
+    @Test
+    public void checkUserAdded() throws URISyntaxException {
+        Users u = new Users();
+        Assert.assertFalse(uc.addUser(u));
+        u.setEmail("testtwo@host.com");
+        u.setPassword("password");
+        Assert.assertFalse(uc.addUser(u));
+    }
+
+
 
 }
