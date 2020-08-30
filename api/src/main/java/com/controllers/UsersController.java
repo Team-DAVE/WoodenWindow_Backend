00077ee64ae6032f090395822e06c9bca5d759e5
Dwood150 <68204916+Dwood150@users.noreply.github.com>
2020-08-30 19:29:54 -0400

    Update UsersController.java

--- api/src/main/java/com/controllers/UsersController.java
+++ api/src/main/java/com/controllers/UsersController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.net.URISyntaxException;
